ecb2355c8b484e690ee4bdb4be279a5f8f7bdb70
arjunth889 <arjunth889@gmail.com>
2022-06-22 17:24:34 +0530

    added card component

--- src/components/Pages/About.svelte
+++ src/components/Pages/About.svelte
@@ -1,14 +1,44 @@
-<script></script>
+<script>
+  import Courses from "../../data/data";
+
+  const { coursedata } = Courses;
+</script>
 
 <div id="about">
-about
+  <div class="card-wraper">
+    {#each coursedata as { id, username, desc }, i}
+      <div class="card">
+        <div class="card-header">
+          <h1>{username}</h1>
+        </div>
+        <div class="card-desc">
+          <h3>
+            {desc}
+          </h3>
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-    div{
-        display: flex;
-        height: 100vh;
-        align-items: center;
-        justify-content: center;
-    }
+  div {
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .card-wraper {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 70vw;
+  }
+  .card {
+    width: 400px;
+    height: 300px;
+    border: 1px solid black;
+    border-radius: 30px;
+  }
 </style>
---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+const Courses ={
+    coursedata :[
+        {
+            id : "1",
+            username: 'rake',
+            desc:'kkk'
+        },
+        {
+            id : "2",
+            username:'kiko',
+            desc: 'ass'
+        }
+    ]
+}
+export default Courses
