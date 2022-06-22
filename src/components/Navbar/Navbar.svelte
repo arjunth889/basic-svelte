4562a1a65889352d9af81f1319af100bc58cb2c6
arjunth889 <arjunth889@gmail.com>
2022-06-22 15:55:59 +0530

    unknows

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -7,3 +7,20 @@
     <a href="/#about"> About</a>
     <a href="/#contact">Contact</a>
 </nav>
+
+<style>
+    nav{
+        position: fixed;
+        top:0;
+        left:0;
+        width: 100%;
+        background: #000;
+        display: flex;
+        justify-content: space-around;
+        
+    }
+    a{
+        text-decoration: none;
+        color: white;
+    }
+</style>
